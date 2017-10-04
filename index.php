e8bfd684baf40524cc96864861a69197d16cf8ae
trix0 <matus.satara@gmail.com>
2017-10-05 01:38:25 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
 <?php
 
-$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbName="exc1";
-	// Create connection
-	$conn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
-
-// Check connection
-	if (!$conn) {
-	    die("Connection failed: " . mysqli_connect_error());
-	}
-
 function fnEncrypt($func,$input_message,$secret_key){
 	//returns encrypted message as array ;
 	$enrypted_message=[];
-	$cipher="";
-
-
 	//return ecb encrypted message;
 	if($func=="ecb"){
 		array_push($enrypted_message,base64_encode(@mcrypt_encrypt('tripledes',$secret_key,$input_message,'ecb'))); // encrypt and push to array
@@ -42,45 +27,22 @@ function fnDecrypt($func,$input_message,$secret_key,$iv="base"){
 	$input_cipher=base64_decode($input_message);
 	
 	//return ecb decrypted message
-		if($func==="ecb"){
-			$decrypted_message=mcrypt_decrypt('tripledes',$secret_key,$input_cipher,'ecb');
-		}
+	if($func==="ecb"){
+		$decrypted_message=mcrypt_decrypt('tripledes',$secret_key,$input_cipher,'ecb');
+	}
 	
 
 	//return cbc decrypted message
-		elseif($func==="cbc"){
-			$decrypted_message=mcrypt_decrypt('tripledes',$secret_key,$input_cipher,'cbc',$iv);
-
-		}
-
-
+	elseif($func==="cbc"){
+		$decrypted_message=mcrypt_decrypt('tripledes',$secret_key,$input_cipher,'cbc',$iv);
+	}
 	//wrong function name
-		else{
-			echo("Wrong function name dec");	
-		}
-		return $decrypted_message;
+	else{
+		echo("Wrong function name dec");	
+	}
+	return $decrypted_message;
 
 }
-
-function fnSaveToDb($message,$iv="notset",$conn){
-
-	$stmt = $conn->prepare("INSERT INTO `message`(`message`, `iv`) VALUES (:message,:iv)");
-	$stmt->bindParam(':message',$message);
-	$stmt->bindParam(':iv', $iv);
-	$stmt->execute();
-}
-
-
-function fnSelectFromDb($id,$conn){
-	$stmt = $conn->prepare("SELECT * FROM message WHERE message_id = :id");
-	$stmt->bindParam(":id", $id);
-	$stmt->execute();
-	$row  = $stmt -> fetch();
-	return $row;
-}
-
-
-
 
 $message="Secret message. Do not share";
 $secretKey="ThisIsASecretKey12345678";
